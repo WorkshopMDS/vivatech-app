12aab20593ad4ac45e3fbe0d047a7b0451ec4908
Kilian Pichard <kilian.pichard@botify.com>
2023-05-03 11:17:03 +0200

    Use Theme on app.tsx and add SafeAreaProvider

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
-import { NavigationContainer } from '@react-navigation/native'
+import {
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
+} from '@react-navigation/native'
+import { useColorScheme } from 'react-native'
 import { useFonts } from 'expo-font'
+import { ThemeProvider } from 'styled-components'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import VivaTheme from './app/Theme'
 import Navigation from './app/Navigation'
 
 export default function App() {
+  const scheme = useColorScheme()
+
+  const isDarkMode = scheme === 'dark'
+
+  const theme = VivaTheme(isDarkMode ? DarkTheme : DefaultTheme)
+
   const [fontsLoaded] = useFonts({
     'Museo-100': require('./assets/fonts/MuseoSans-100.otf'),
     'Museo-300': require('./assets/fonts/MuseoSans-300.otf'),
@@ -16,8 +30,12 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <Navigation />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <ThemeProvider {...{ theme }}>
+        <NavigationContainer {...{ theme }}>
+          <Navigation />
+        </NavigationContainer>
+      </ThemeProvider>
+    </SafeAreaProvider>
   )
 }
