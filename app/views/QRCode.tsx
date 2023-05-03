35835b404d9f15d9ccf7b96de3ca56e538457395
Kilian Pichard <kilian.pichard@botify.com>
2023-05-03 11:17:32 +0200

    Use colors on view and fonts for QR Code Title + Fix SafeAreaView

--- app/views/QRCode.tsx
+++ app/views/QRCode.tsx
@@ -1,9 +1,18 @@
-import { Text, SafeAreaView } from 'react-native'
+import { Text } from 'react-native'
+import styled from 'styled-components'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Title = styled(Text)`
+  font-family: Museo-500;
+  font-size: 24px;
+  color: ${({ theme }) => theme.colors.primary100};
+  text-align: center;
+`
 
 function QRCode() {
   return (
     <SafeAreaView>
-      <Text>QRCode</Text>
+      <Title>QRCode</Title>
     </SafeAreaView>
   )
 }
