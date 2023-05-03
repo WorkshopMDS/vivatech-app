792c8372c3f52efc6472529eeb9113eab87242c2
Kilian Pichard <kilian.pichard@botify.com>
2023-05-03 11:15:18 +0200

    Import Fonts from VivaTech Design

--- App.tsx
+++ App.tsx
@@ -1,7 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native'
+import { useFonts } from 'expo-font'
 import Navigation from './app/Navigation'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Museo-100': require('./assets/fonts/MuseoSans-100.otf'),
+    'Museo-300': require('./assets/fonts/MuseoSans-300.otf'),
+    'Museo-500': require('./assets/fonts/MuseoSans_500.otf'),
+    'Museo-700': require('./assets/fonts/MuseoSans_700.otf'),
+    'Museo-900': require('./assets/fonts/MuseoSans_900.otf'),
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <NavigationContainer>
       <Navigation />
