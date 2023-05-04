61da7fbd8a6d4f4bee567538956938df6fd454d9
Kilian Pichard <kilian.pichard@botify.com>
2023-05-04 11:13:55 +0200

    Update Config and Logo changes on theme

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -1,14 +1,18 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { AntDesign } from '@expo/vector-icons'
-import { View, Image } from 'react-native'
+import { View, Image, useColorScheme } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 import Home from './views/Home'
 import { useCustomTheme } from './utils/Theme'
 
 import QRCodeModal from './components/QRCode/QRCodeModal'
 import useToggle from './utils/useToggle'
+import QRCodeScannerView from './views/QRCodeScannerView'
 
-const logo = require('../assets/full_small.png')
+const logos = {
+  dark: require('../assets/header_dark.png'),
+  light: require('../assets/header_light.png'),
+}
 
 const Tab = createBottomTabNavigator()
 
@@ -18,6 +22,9 @@ function MyModalBackgroundScreen() {
 
 function Navigation() {
   const { colors } = useCustomTheme()
+  const scheme = useColorScheme()
+  const isDarkMode = scheme === 'dark'
+  const logo = logos[isDarkMode ? 'dark' : 'light']
 
   const [isOpen, toggle] = useToggle()
 
