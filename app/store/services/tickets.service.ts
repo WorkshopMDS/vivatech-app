e36894155c126bb49983ebbd5a7a2302ed446d80
Kilian Pichard <kilian.pichard@botify.com>
2023-05-23 16:10:42 +0200

    remove axios

--- app/store/services/tickets.service.ts
+++ app/store/services/tickets.service.ts
@@ -1,22 +1,19 @@
-import axios from 'axios'
-
 const API_URL = 'https://viva-api.fly.dev'
 const ENDPOINT = '/ticket'
 
 export const validateTicketService = (ticket: string) =>
-  axios
-    .get(`${API_URL + ENDPOINT}/validation/${ticket}`)
+  fetch(`${API_URL}${ENDPOINT}/validation/${ticket}`)
+    .then(response => response.json())
     .then(response => {
       if (response.data) {
         return {
           ticket,
-          user: response.data.data.user,
+          user: response.data.user,
         }
       }
       return Promise.reject()
     })
     .catch(error => {
-      console.log(API_URL + ENDPOINT)
       console.log(error)
       return Promise.reject(error)
     })
