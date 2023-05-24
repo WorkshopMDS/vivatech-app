c4fee070278c7f36eb1d15bb2fabbea938c0e649
Kilian Pichard <kilian.pichard@botify.com>
2023-05-24 11:01:44 +0200

    Add CV Store

--- app/store/reducers/index.ts
+++ app/store/reducers/index.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import ticketsReducer from './tickets.reducer'
+import cvReducer from './cv.reducer'
 
 export default combineReducers({
   tickets: ticketsReducer,
+  cv: cvReducer,
 })
