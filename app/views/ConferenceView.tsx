a4adb9c7a75dd10bb0fcab664a5e634f3f70f26f
iti98 <etienne98.ch@gmail.com>
2023-05-25 11:12:42 +0200

    Pour assister aux trials

--- app/views/ConferenceView.tsx
+++ app/views/ConferenceView.tsx
@@ -1,21 +1,53 @@
-import { FlatList, View } from 'react-native'
-
+import { FlatList, View, Text, Pressable, TextInput } from 'react-native'
+import { useState } from 'react'
 import styled from 'styled-components'
+import { AntDesign } from '@expo/vector-icons'
 import ConferenceCard from '../components/Conference/ConferenceCard'
-import { useAppSelector } from '../hooks'
+import { useAppSelector, useToggle } from '../hooks'
+import { IConference } from '../models/ConferenceType'
 
 const Container = styled(View)`
   flex: 1;
 `
 
+const FilterConferences = styled(Pressable)`
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  padding-right: 20px;
+  padding-top: 10px;
+`
+const FilterContent = styled(View)<{ isFilterOpen: boolean }>`
+  height: ${({ isFilterOpen }) => (isFilterOpen ? 'auto' : '0')};
+  transition: height 7s;
+  overflow: hidden;
+`
+
 function ConferenceList() {
   const { conferences } = useAppSelector(state => state.conferences)
 
+  const [searchedValue, onSearchedValueChange] = useState('')
+
+  const filteredData = searchedValue
+    ? conferences.filter((item: IConference) =>
+        item.speaker?.name?.startsWith(searchedValue),
+      )
+    : conferences
+
+  const [isFilterOpen, toggleFilter] = useToggle(false)
+
   return (
     <Container>
+      <FilterConferences onPress={toggleFilter}>
+        <AntDesign name="filter" size={30} color="white" />
+      </FilterConferences>
+      <FilterContent isFilterOpen={isFilterOpen}>
+        <TextInput onChangeText={onSearchedValueChange} value={searchedValue} />
+        <Text>Sort</Text>
+      </FilterContent>
       <FlatList
         contentContainerStyle={{ paddingBottom: 100 }}
-        data={conferences}
+        data={filteredData}
         renderItem={({ item }) => <ConferenceCard conference={item} />}
       />
     </Container>
